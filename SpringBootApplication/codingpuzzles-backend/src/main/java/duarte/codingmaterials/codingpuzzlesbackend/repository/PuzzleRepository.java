1cd40e543bef1c58149bf53a74cce3e25b9cefd7
Duarte Cardoso <caldasdcardoso@gmail.com>
2024-02-13 10:35:24 +0000

    Adding the puzzleRepository class, extending JPA repository with the puzzle entity

---
+++ SpringBootApplication/codingpuzzles-backend/src/main/java/duarte/codingmaterials/codingpuzzlesbackend/repository/PuzzleRepository.java
@@ -0,0 +1,8 @@
+package duarte.codingmaterials.codingpuzzlesbackend.repository;
+
+import duarte.codingmaterials.codingpuzzlesbackend.entity.Puzzle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PuzzleRepository extends JpaRepository<Puzzle,Long> {
+
+}
