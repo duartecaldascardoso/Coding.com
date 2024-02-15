fe33f85cb2be44c19d15cf4c77a1b87b14b1496b
Duarte Cardoso <caldasdcardoso@gmail.com>
2024-02-15 19:44:10 +0000

    Adding basic testing for the puzzle constructor

--- SpringBootApplication/codingpuzzles-backend/src/test/java/duarte/codingmaterials/codingpuzzlesbackend/CodingPuzzlesBackendApplicationTests.java
+++ SpringBootApplication/codingpuzzles-backend/src/test/java/duarte/codingmaterials/codingpuzzlesbackend/CodingPuzzlesBackendApplicationTests.java
@@ -1,5 +1,6 @@
 package duarte.codingmaterials.codingpuzzlesbackend;
 
+import duarte.codingmaterials.codingpuzzlesbackend.entity.Puzzle;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,14 @@ class CodingPuzzlesBackendApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void puzzleConstructorTest(){
+		Puzzle puzzle = new Puzzle(1L,"What is the first position of an array?", "B");
+
+		assert(puzzle.getId().equals(1L));
+		assert(puzzle.getAnswer().equals("B"));
 	}
 
 }
