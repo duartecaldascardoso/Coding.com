ceab9c275f4231ebbe9fd714a7c2599910739713
Duarte Cardoso <caldasdcardoso@gmail.com>
2024-02-18 10:47:47 +0000

    Fixing and adding more tests to the solution

--- SpringBootApplication/codingpuzzles-backend/src/test/java/duarte/codingmaterials/codingpuzzlesbackend/CodingPuzzlesBackendApplicationTests.java
+++ SpringBootApplication/codingpuzzles-backend/src/test/java/duarte/codingmaterials/codingpuzzlesbackend/CodingPuzzlesBackendApplicationTests.java
@@ -1,22 +1,34 @@
 package duarte.codingmaterials.codingpuzzlesbackend;
 
+import duarte.codingmaterials.codingpuzzlesbackend.dto.PuzzleDto;
 import duarte.codingmaterials.codingpuzzlesbackend.entity.Puzzle;
+import duarte.codingmaterials.codingpuzzlesbackend.mapper.PuzzleMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CodingPuzzlesBackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
-	@Test
-	public void puzzleConstructorTest(){
-		Puzzle puzzle = new Puzzle(1L,"What is the first position of an array?", "B");
+    @Test
+    public void puzzleConstructorTest() {
+        Puzzle puzzle = new Puzzle(1L, "What is the first position of an array?", "B", "0");
 
-		assert(puzzle.getId().equals(1L));
-		assert(puzzle.getAnswer().equals("B"));
+        assert (puzzle.getId().equals(1L));
+        assert (puzzle.getAnswer().equals("B"));
+        assert (puzzle.getAnswerString().equals("0"));
+    }
+
+    @Test
+    public void puzzleMapperTest() {
+		Puzzle puzzle = new Puzzle(1L, "What is the first position of an array?", "B", "0");
+		PuzzleDto puzzleDto = new PuzzleDto(1L, "What is the first position of an array?", "B", "0");
+
+		assert (PuzzleMapper.mapToPuzzle(puzzleDto).getAnswer().equals(puzzle.getAnswer()));
+		assert (PuzzleMapper.mapToPuzzleDto(puzzle).getAnswer().equals(puzzleDto.getAnswer()));
 	}
 
 
