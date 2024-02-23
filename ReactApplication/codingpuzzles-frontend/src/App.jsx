e8483d570597aa368438407cf2c67e81f988ff68
Duarte Cardoso <caldasdcardoso@gmail.com>
2024-02-23 15:30:20 +0000

    Adding header component to the application

--- ReactApplication/codingpuzzles-frontend/src/App.jsx
+++ ReactApplication/codingpuzzles-frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import HeaderComponent from './components/HeaderComponent'
 import ListPuzzlesComponent from './components/ListPuzzlesComponent'
 
 function App() {
 
   return (
+    <>
+    <HeaderComponent />
     <ListPuzzlesComponent />
+    </>
+    
   )
 }
 
