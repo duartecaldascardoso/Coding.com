f0994a81b56800b7b76c10c0dfbb7ecddc2b815a
caldas33 <126091251+caldas33@users.noreply.github.com>
2024-02-29 16:03:26 +0000

    Adding the user class, will be implementing the rest

---
+++ SpringBootApplication/codingpuzzles-backend/src/main/java/duarte/codingmaterials/codingpuzzlesbackend/entity/User.java
@@ -0,0 +1,23 @@
+package duarte.codingmaterials.codingpuzzlesbackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "eloId", nullable = false)
+    private String eloId;
+}
+
