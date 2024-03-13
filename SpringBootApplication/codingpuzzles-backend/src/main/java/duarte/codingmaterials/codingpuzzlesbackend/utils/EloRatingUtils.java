66660af4d4d1d26846cd55051a70ad50b34d4350
caldas33 <126091251+caldas33@users.noreply.github.com>
2024-03-13 18:06:52 +0000

    Starting the elo rating utils logic

--- SpringBootApplication/codingpuzzles-backend/src/main/java/duarte/codingmaterials/codingpuzzlesbackend/utils/EloRatingUtils.java
+++ SpringBootApplication/codingpuzzles-backend/src/main/java/duarte/codingmaterials/codingpuzzlesbackend/utils/EloRatingUtils.java
@@ -2,5 +2,9 @@ package duarte.codingmaterials.codingpuzzlesbackend.utils;
 
 public class EloRatingUtils {
 
+    private static final Integer INITIAL_USER_RATING = 400;
 
+    public int calculateEloForUser(){
+        return 0;
+    }
 }
