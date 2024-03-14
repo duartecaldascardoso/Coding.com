bd379b98e1c33a76ee88cb256828a715cb3d806c
caldas33 <126091251+caldas33@users.noreply.github.com>
2024-03-14 13:55:54 +0000

    Improving the elo calculation method

--- SpringBootApplication/codingpuzzles-backend/src/main/java/duarte/codingmaterials/codingpuzzlesbackend/utils/EloRatingUtils.java
+++ SpringBootApplication/codingpuzzles-backend/src/main/java/duarte/codingmaterials/codingpuzzlesbackend/utils/EloRatingUtils.java
@@ -3,8 +3,11 @@ package duarte.codingmaterials.codingpuzzlesbackend.utils;
 public class EloRatingUtils {
 
     private static final Integer INITIAL_USER_RATING = 400;
+    private double timePassed;
+    private static final double RATING_COEFFICIENT = 0.3;
 
-    public int calculateEloForUser(){
+    public int calculateEloForUser(Double timePassed, int currentRating){
+
         return 0;
     }
 }
